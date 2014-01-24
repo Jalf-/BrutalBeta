343981a9fd7829ebd109d9fb95dae1a7ad3def9f
Jalf- <benjaminboendergaard@hotmail.com>
2014-01-24 09:58:19 +0100

    Update everything

--- BrutalBeta/src/com/company22/brutalbeta/MainActivity.java
+++ BrutalBeta/src/com/company22/brutalbeta/MainActivity.java
@@ -1,36 +1,154 @@
 package com.company22.brutalbeta;
 
+import java.util.Formatter;
+import java.util.Random;
 import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity
+{
+	// Make variables.
+	private float beta;
+	private float lambda;
+	private float temperature;
+	private SeekBar roomTempSlider;
+	private TextView roomTempNumber;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		double beta = 17.502;
-		double lambda = 240.97;
+		// Initialize variables.
+		beta = 17.502f;
+		lambda = 240.97f;
 		
-		//Get current value and display as string
-		TextView textView = (TextView) findViewById(R.id.textView1);
-		textView.setText(String.valueOf(19)); //Output Value/variable
-		//Cannot read variable from TimerTask Void-class, but it goes above
-		
-		Timer t = new Timer();
-		t.schedule(new com.company22.brutalbeta.TimerTask(beta, lambda), 10000, 10000);
+		// Start listeners.
+		// Slider listener.
+		roomTempSlider = (SeekBar) findViewById(R.id.roomTempSlider);
+		roomTempSlider.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+		{
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+			{
+				roomTempNumber = (TextView) findViewById(R.id.roomTempNumber);
+				roomTempNumber.setText(String.valueOf((Float.parseFloat(String.valueOf(progress)) / 10) + 10));
+			}
+		});
+
+		// Start timer.
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask()
+		{	
+			@Override
+			public void run()
+			{
+				TimerMethod();
+			}
+		}, 1000, 1000);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
+	/**
+	 * Method to run the timer in the main class.
+	 */
+	private void TimerMethod()
+	{
+		this.runOnUiThread(TimerTick);
+	}
+	
+	/**
+	 * Timer that will update and calculate the result.
+	 */
+	private Runnable TimerTick = new Runnable()
+	{
+		@Override
+		public void run()
+		{
+			// Get input data.
+			
+			// Try to get room temperature from the slider.
+			try
+			{			
+				roomTempSlider = (SeekBar) findViewById(R.id.roomTempSlider);
+				temperature = (Float.parseFloat(String.valueOf(roomTempSlider.getProgress())) / 10) + 10;
+			}
+			catch (Exception e)
+			{
+				temperature = 20;
+			}
+			Random random = new Random();
+			float humidity = random.nextInt(99) + random.nextFloat();
+			
+			// Dew point calculation.
+			float parentheses = (float) (Math.log(humidity / 100) + (getBeta() * temperature) / (getLambda() + temperature));
+			float dewPoint = (getLambda() * parentheses) / (getBeta() - parentheses);
+			
+			// Format output.
+			Formatter formatter = new Formatter();
+			String dewPointString = formatter.format("%.1f", dewPoint).out().toString();
+			formatter.close();
+			
+			// Output.
+			TextView dewPointText = (TextView) findViewById(R.id.dewPointTextView);
+			dewPointText.setText(dewPointString);
+
+			System.out.println("Temperature: " + temperature + " Humidity: " + humidity + " Dew point: " + dewPoint);
+		}
+	};
+
+	/**
+	 * @return Beta value.
+	 */
+	public float getBeta()
+	{
+		return beta;
+	}
+
+	/**
+	 * Sets the beta value.
+	 * @param beta New beta value.
+	 */
+	public void setBeta(float beta)
+	{
+		this.beta = beta;
+	}
+
+	/**
+	 * @return Lambda value.
+	 */
+	public float getLambda()
+	{
+		return lambda;
+	}
+
+	/**
+	 * Sets the lambda value.
+	 * @param lambda New lambda value.
+	 */
+	public void setLambda(float lambda)
+	{
+		this.lambda = lambda;
+	}
 }
